c256764d5f6ca6470791d0f9d6d453cb77b98fa1
Tiuters <ands458@gmail.com>
2023-10-17 14:38:35 +0300

    OK

---
+++ bookmarker-api/src/main/java/com/example/bookmarkerapi/domain/BookmarkService.java
@@ -0,0 +1,28 @@
+package com.example.bookmarkerapi.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BookmarkService {
+
+    private final BookmarkRepository bookmarkRepository;
+    private final BookmarkMapper bookmarkMapper;
+
+    @Transactional(readOnly = true)
+    public BookmarksDTO getBookmarks(Integer page) {
+        int pageNo = page < 1 ? 0 : (page - 1);
+        PageRequest pageRequest = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "createdAt");
+        Page<BookmarkDTO> bookmarkPage = bookmarkRepository.findBookmarks(pageRequest);
+        return new BookmarksDTO(bookmarkPage);
+    }
+
+}
